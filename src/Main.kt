cbf905a9d69ef2a0a2ac718fe314870b44d752b1
Cesar Carrasco <ccarrascocarre@gmail.com>
2026-01-02 23:17:37 -0300

    Null safety example

--- src/Main.kt
+++ src/Main.kt
@@ -9,12 +9,12 @@ fun main(){
 
     // Manera larga de generar una variable
     var v1 : Int = 1
-    print(v1)
+    println(v1)
 
     // Esta es la segunda forma de declarar un valor, sin embargo, aca es Kotlin dependiendo del tipo de datos que se
     // asigne será el tipo de dato que designe.
     var v2 = 2.1
-    print(v2)
+    println(v2)
 
     // Este error no da un error porque nos el mismo tipo de dato, no se puede reasignar el tipo de variable como en
     // python o javascript
@@ -31,7 +31,25 @@ fun main(){
     // Una constante siempre van a tener un mismo valor, en vez de generar var uno tendría que ponder val, la diferencia
     // entre estos dos tipos es que var utiliza menos memoria a diferencia de val, es como una mochila que puede
     // almacenar muchas cosas diferentes, sin embargo, la constante va a tener un empaque mucho más óptimo.
+    // Siempre utilizar constantes si sabemos que el valor no cambiará
     val pi = 3.14
 
-    // Siempre utilizar constantes si sabemos que el valor no cambiará
+    // ================ Null safety / valores opcionales =========================
+    // Variable que pueden ir cambiando el valor mediante eje
+    // var x = null
+    var x : String? = null
+    x = "Hola César Carrasco!"
+    println(x)
+
+    // De esta forma podemos realizar operaciones cuando el valor realmente exista
+    x?.let{
+        println("tiene valor")
+    }
+
+    // Pantalla a y b quiero enviar de listado de usuarios a detalle de usuario
+    var id : Int? = null
+    id?.let {
+        println("Valor id: $it")
+    }
+
 }
