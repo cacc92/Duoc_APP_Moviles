4524cc38f9c93dff0d4f244e8a1701c5b1a8c671
Cesar Carrasco <ccarrascocarre@gmail.com>
2026-01-02 22:28:44 -0300

    Se genera introducción de variables

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,30 @@
 fun main(){
     println("Hola Mundo!")
+
+    // ========================= VARIABLES =====================================
+    /*
+    * Podemos utilizar diferentes tipos de variables por ejemplo
+    * String (text) Int (enteros) Double (decimales más precisos) Float (Decimales) Boolean (estado true or false)
+    * */
+
+    // Manera larga de generar una variable
+    var v1 : Int = 1
+    print(v1)
+
+    // Esta es la segunda forma de declarar un valor, sin embargo, aca es Kotlin dependiendo del tipo de datos que se
+    // asigne será el tipo de dato que designe.
+    var v2 = 2.1
+    print(v2)
+
+    // Este error no da un error porque nos el mismo tipo de dato, no se puede reasignar el tipo de variable como en
+    // python o javascript
+    // v2 = "Hola"
+
+    v2 = 4.5
+
+    // Puedo concatenar variables
+    var texto1 = "Hola "
+    var texto2 = "Mundo"
+    println(texto1 +  texto2)
+
 }
